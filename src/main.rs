157cbb0ca182c9051ec75c6d81a30418b130eb1d
Thales Macedo Garitezi <thalesmg@gmail.com>
2023-07-14 20:05:36 -0300

    formatando queries

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use tree_sitter::{Language, Parser, Query, QueryCursor};
+use tree_sitter::{Language, Parser, Query, QueryCapture, QueryCursor};
 
 use clap::{arg, value_parser, Command};
 use ignore::Walk;
@@ -13,14 +13,14 @@ fn main() {
 
     let matches = Command::new("scryer")
         .arg(
-            arg!(-r --root <ROOT> "Path of the root of the project")
-                .default_value(".")
-                // .value_parser(value_parser!(PathBuf))
+            arg!(-r --root <ROOT> "Path of the root of the project").default_value("."), // .value_parser(value_parser!(PathBuf))
         )
-        .arg(arg!(
-            -q --query <QUERY> "Tree-sitter query"
+        .arg(
+            arg!(
+                -q --query <QUERY> "Tree-sitter query"
+            )
+            .required(true),
         )
-             .required(true))
         .get_matches();
 
     let root = matches.get_one::<String>("root").unwrap();
@@ -41,23 +41,40 @@ fn main() {
                     println!("whole tree:\n  {}", root.to_sexp());
                     let query = Query::new(lang, query_source).unwrap();
                     let mut cursor = QueryCursor::new();
-                    for m in cursor.matches(&query, root, source.as_bytes()) {
-                        println!("query match: {:?}", m);
+                    let mut matches = cursor.matches(&query, root, source.as_bytes()).peekable();
+                    if matches.peek().is_some() {
+                        // println!("{}:", p.file_name().map(|o| o.to_str()).flatten().unwrap_or("⁉⁉⁉"));
+                        println!("{}:", p.to_path_buf().as_path().to_str().unwrap_or("⁉⁉⁉"));
+                    }
+                    for m in matches {
+                        // println!("query match: {:?}", m);
+                        let capture_names = query.capture_names();
                         for cap in m.captures {
-                            println!("capture: {:?}", cap);
-                            println!("capture text: {:?}", cap.node.utf8_text(source.as_bytes()));
+                            // println!("capture: {:?}", cap);
+                            // println!("capture node: {:?}", cap.node);
+                            // println!("capture text: {:?}", cap.node.utf8_text(source.as_bytes()));
+                            // println!("capture name: {:?}", &capture_names[cap.index as usize]);
+                            format_capture(&cap, &capture_names, &source);
                         }
                     }
-                },
+                }
                 _ => continue,
             }
         }
     }
 }
 
+fn format_capture(capture: &QueryCapture, capture_names: &[String], source: &str) {
+    let line_num = capture.node.start_position().row;
+    let cap_name = &capture_names[capture.index as usize];
+    let cap_text = capture.node.utf8_text(source.as_bytes()).unwrap_or("⁈⁈⁈");
+    println!("  {}:", cap_name);
+    println!("    {}: {}", line_num, cap_text);
+}
+
 #[cfg(test)]
 mod tests {
-    use tree_sitter::{Parser, Query, QueryCursor, Point, TreeCursor};
+    use tree_sitter::{Parser, Point, Query, QueryCursor, TreeCursor};
 
     use crate::tree_sitter_erlang;
 
@@ -111,7 +128,10 @@ mod tests {
                 println!("capture: {:?}", cap);
                 println!("capture text: {:?}", cap.node.utf8_text(source.as_bytes()));
                 println!("capture parent: {:?}", cap.node.parent());
-                println!("capture parent child count: {:?}", cap.node.parent().unwrap().child_count());
+                println!(
+                    "capture parent child count: {:?}",
+                    cap.node.parent().unwrap().child_count()
+                );
                 let mut ccursor = cap.node.parent().unwrap().walk();
                 let mut success = ccursor.goto_first_child();
                 while success {
@@ -128,14 +148,12 @@ mod tests {
                 .matches(&query, root, source.as_bytes())
                 .into_iter()
                 .flat_map(|m| {
-                    m.captures
-                        .into_iter()
-                        .map(|c| {
-                            let spos = c.node.start_position();
-                            let epos = c.node.end_position();
-                            let atom_name = c.node.utf8_text(source.as_bytes()).unwrap();
-                            (atom_name, (spos, epos))
-                        })
+                    m.captures.into_iter().map(|c| {
+                        let spos = c.node.start_position();
+                        let epos = c.node.end_position();
+                        let atom_name = c.node.utf8_text(source.as_bytes()).unwrap();
+                        (atom_name, (spos, epos))
+                    })
                 })
                 .collect::<Vec<_>>()
         );
